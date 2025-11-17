0bdec85dddb6837ed338d6366367efa456918500
Melik Samed ALMACI <almaci66@gmail.com>
2025-11-17 23:06:23 +0300

    V17.11
    Merte Gösteri Yapılıd

--- MarketStockTracking/MarketStockTracking/obj/Debug/net9.0-windows/MarketStockTracking.AssemblyInfo.cs
+++ MarketStockTracking/MarketStockTracking/obj/Debug/net9.0-windows/MarketStockTracking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketStockTracking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fab31fe6a513e463e41ff4cc1ff2eacd7296f89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e25a7092b811e714ebccc77b49507829490f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketStockTracking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketStockTracking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
